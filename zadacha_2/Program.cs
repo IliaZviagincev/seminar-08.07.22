7dc1b5809190190437764c491fd5f3b0205562ae
Ilia_Z <akinoro19@gmail.com>
2022-07-08 23:11:19 +0300

    add zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число:  ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:  ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число первое больше второго:  " + num1 + " > " +num2);
+}
+else
+{
+    Console.WriteLine("Число второе больше первого:  " + num1 + " < " + num2);
+}
