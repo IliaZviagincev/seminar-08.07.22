79b9b410f32df73e9af316bee76f8ce0b1a1e058
Ilia_Z <akinoro19@gmail.com>
2022-07-09 01:57:47 +0300

    add zadacha_6

---
+++ zadacha_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите для проверки его четности:  ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Да,оно четное");
+}
+else
+{
+    Console.WriteLine("Нет,оно не четное");
+}
