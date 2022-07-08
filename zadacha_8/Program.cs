8475700491f7a87e013d865a1fa773dc0a64c052
Ilia_Z <akinoro19@gmail.com>
2022-07-09 02:27:48 +0300

    add zadacha_8

---
+++ zadacha_8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число:  ");
+int num = int.Parse(Console.ReadLine());
+int i;
+if (num > 0) // если число положительное
+{
+    for (i = 2; i <= num; i += 2)
+    {
+        Console.Write(" " + i);
+    }
+}
+if (num==0) // а вдруг пользователь ввел нолик
+{
+    Console.Write("Зачем Вы ввели нуль?");
+}
+else // а здесь мы полагаем, что число может быть отрицательным
+{
+    for (i=-2;i >= num; i=i-2)
+    {
+        Console.Write(" "+i);
+    }
+}
